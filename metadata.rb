6ab994e005beb228193676addf94763be5069b96
Thiago Dias <tdtadeu@gmail.com>
2014-09-22 14:26:56 -0300

    Touching version and template for zshrc

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ name "oh_my_zsh"
 license "MIT"
 description "A chef recipe to install oh-my-zsh"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.0.1"
+version "0.0.3"
 
 depends 'git'

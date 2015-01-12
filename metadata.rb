e46eb56951ca92a2dd4e5a10c6a19cbad5a2ee8f
Thiago Dias <tdtadeu@gmail.com>
2015-01-12 15:20:00 -0200

    export GEM_HOME so bundle runs without sudo

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ name "oh_my_zsh"
 license "MIT"
 description "A chef recipe to install oh-my-zsh"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version "0.0.6"
+version "0.0.7"
 
 depends "git"
 depends "zsh"
